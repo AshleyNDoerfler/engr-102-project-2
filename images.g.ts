4550643d3328b55fb1302c2617a5343b4dbaeafb
Ashley Doerfler <73670703+AshleyNDoerfler@users.noreply.github.com>
2022-02-01 13:25:45 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . 3 3 3 3 . . . . . . . . . . 
+. 3 3 3 3 3 3 . . . . . . . . . 
+3 1 f 3 1 f 3 3 . . . . . . . . 
+3 1 1 3 1 1 3 3 3 . . . . . . . 
+3 3 3 4 3 3 3 3 3 3 . . . . . . 
+3 3 4 4 4 3 3 3 3 3 3 . . . . . 
+. 3 3 3 3 3 3 3 3 3 3 3 . . . . 
+. . 3 f 3 3 . 3 3 3 3 3 3 . . . 
+. . . . . . . 3 3 3 3 3 3 3 3 . 
+. . . . . . 3 3 4 3 3 4 3 3 3 3 
+. . . . . 3 3 3 3 3 3 3 3 3 3 3 
+. . . . 3 3 3 3 3 3 3 3 3 3 3 3 
+. . . 3 3 . . 3 3 4 3 4 3 3 . 3 
+. . . . . . . 3 3 . 4 . 3 3 . 3 
+. . . . . . 3 3 3 . . 3 3 3 . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "Star":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . d . . . . . . . 
+. . . . . . . d 1 d . . . . . . 
+. . . . . . . 4 1 4 . . . . . . 
+. . . . . . . 4 5 4 . . . . . . 
+. . . . . . 4 d 5 d 4 . . . . . 
+. . . d 4 4 d 5 5 5 d 4 4 d . . 
+. . d 1 1 5 5 5 5 5 5 5 1 1 d . 
+. . . d 4 4 d 5 5 5 d 4 4 d . . 
+. . . . . . 4 d 5 d 4 . . . . . 
+. . . . . . . 4 5 4 . . . . . . 
+. . . . . . . 4 1 4 . . . . . . 
+. . . . . . . d 1 d . . . . . . 
+. . . . . . . . d . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
